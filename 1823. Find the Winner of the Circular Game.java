1ad0e6838776ac3465bdfbec4130c8cf0c622f34
hiteshshimpi <80444596+hiteshshimpi-55@users.noreply.github.com>
2022-01-25 21:06:24 +0530

    Update 1823. Find the Winner of the Circular Game.java

--- 1823. Find the Winner of the Circular Game.java
+++ 1823. Find the Winner of the Circular Game.java
@@ -36,3 +36,19 @@ class Solution {
         list.remove(0);
     }
 }
+
+//  public int findTheWinner(int n, int k) {
+     
+//         List<Integer>ans=new ArrayList<>();
+//         for(int i=1;i<=n;i++){
+//             ans.add(i);
+//         }
+//         int cur=0;
+//         while(ans.size()>1){
+//             cur=(cur+k-1)%ans.size();
+//             ans.remove(cur);
+//         }
+//         return ans.get(0);
+//     }
+
+// }
